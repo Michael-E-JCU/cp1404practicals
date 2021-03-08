ea52bc2503458f2f7effc213c5fc15519544df35
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-08 13:27:02 +1100

    Refactor password check program to use functions

--- prac_03/psasword_checker.py
+++ prac_03/psasword_checker.py
@@ -2,12 +2,21 @@ MIN_LEN = 10
 
 
 def main():
-    password = input("Enter password here: ")
+    password = get_password()
     while not get_valid_password(password):
         print("Invalid paassword length! Must be at least 10 characters.")
-        password = input("Enter password here: ")
+        password = get_password()
+    get_asterisks(password)
+
+
+def get_asterisks(password):
     for l in password:
         print("*", end="")
+
+
+def get_password():
+    password = input("Enter password here: ")
+    return password
 
 
 def get_valid_password(password):
