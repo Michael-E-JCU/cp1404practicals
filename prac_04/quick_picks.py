a921d205bb032b94833570197e5bad28f6e63d0f
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-17 20:05:10 +1100

    Add constant for amount of numbers for each list and also add .gitignore

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,19 +4,21 @@ CP1404/CP5632 - Practical
 """
 import random
 
+NUMBER_OF_CYCLES = 6
+
 
 def main():
     number_of_picks = int(input("How many quick picks? "))
-    LISTS = [generate_list_of_6() for i in range(number_of_picks)]
+    LISTS = [generate_list_of_6_random_numbers() for i in range(number_of_picks)]
     for number_list in LISTS:
         for i in number_list:
             print(f"{i:2}", end=" ")
         print("")
 
 
-def generate_list_of_6():
+def generate_list_of_6_random_numbers():
     numbers_list = []
-    for i in range(6):
+    for i in range(NUMBER_OF_CYCLES):
         number = random.randint(1, 45)
         while number in numbers_list:
             number = random.randint(1, 45)
