c3f8f8d84c24f32069e1d932fbd16ab2a00da643
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-24 16:24:51 +1100

    Fix average number printout, add while loop while number < 0

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,15 +3,19 @@ CP1404/CP5632 - Practical
 """
 
 numbers = []
-for i in range(5):
-    number = int(input("Enter a number here: "))
+count = 1
+number = int(input(f"Enter a number {count}: "))
+numbers.append(number)
+while number > 0:
+    count += 1
+    number = int(input(f"Enter a number {count}: "))
     numbers.append(number)
 
 print(f"fThe first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The biggest number is {max(numbers)}")
-print(f"The average number is {sum(numbers) / sum(numbers)}")
+print(f"The average number is {sum(numbers) / len(numbers)}")
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
