7540134ae34e85925b5a9628ac78f444f4b39370
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-03-30 10:55:12 +1100

    Complete emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,13 +5,18 @@ CP1404/CP5632 Practical
 
 
 def main():
-    emails = {"michael.excell@my.jcu.edu.au": "Michael Excell"}
+    emails = {}
     input_email = input("Email: ").lower()
-    if input_email in emails:
-        confirm_name = input(f"Is your name {emails.get(input_email)} (Y/N): ").lower()
+    while input_email != "":
+        split_email = input_email.split("@")
+        name = split_email[0].title().replace(".", " ")
+        emails.update({input_email: name})
+        confirm_name = input(f"Is your name {emails.get(input_email)}? (Y/N): ").lower()
         if confirm_name == "n":
-            name = input("Enter name here: ")
-            emails[input_email[0]].append(name)
-    print(emails)
+            confirm_name = input("Name: ").title()
+            emails[input_email] = confirm_name
+        input_email = input("Email: ").lower()
+    for email in emails:
+        print(f"{emails.get(email)} ({email})")
 
 main()
