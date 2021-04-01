bcc263cfd56f478823b54ca025d0dec28a7e8428
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-04-01 12:08:48 +1100

    Refactor input variable name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,10 +7,10 @@ HEX_COLOURS = {"DarkOrchid4": "#68228b", "DeepPink1": "#ff1493", "black": "	#000
                "firebrick2": "#ee2c2c", "GhostWhite": "#f8f8ff", "green1": "#00ff00", "yellow1": "#ffff00",
                "SlateBlue4": "#473c8b", "orange1": "#ffa500"}
 
-response = input("Enter colour: ")
-while response != "":
-    if response in HEX_COLOURS:
-        print(f"{response}: {HEX_COLOURS[response]}")
+colour = input("Enter colour: ")
+while colour != "":
+    if colour in HEX_COLOURS:
+        print(f"{colour}: {HEX_COLOURS[colour]}")
     else:
         print("colour not in dictionary.")
-    response = input("Enter colour: ")
+    colour = input("Enter colour: ")
