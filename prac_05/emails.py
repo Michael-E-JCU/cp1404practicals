2984b995b5ba70fd6d9f5dc44d580c184e43fec0
Uni-Laptop <michael.excell@my.jcu.edu.au>
2021-04-01 12:19:14 +1100

    Refactor email input, Add error check for confirm_name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,17 +6,22 @@ CP1404/CP5632 Practical
 
 def main():
     emails = {}
-    input_email = input("Email: ").lower()
-    while input_email != "":
-        split_email = input_email.split("@")
+    email = input("Email: ").lower()
+    while email != "":
+        split_email = email.split("@")
         name = split_email[0].title().replace(".", " ")
-        emails.update({input_email: name})
-        confirm_name = input(f"Is your name {emails.get(input_email)}? (Y/N): ").lower()
+        emails.update({email: name})
+        confirm_name = input(f"Is your name {emails.get(email)}? (Y/N): ").lower()
+        while confirm_name != "n" and confirm_name != "y":
+            print("INVALID OPTION!")
+            print(confirm_name)
+            confirm_name = input(f"Is your name {emails.get(email)}? (Y/N): ").lower()
         if confirm_name == "n":
             confirm_name = input("Name: ").title()
-            emails[input_email] = confirm_name
-        input_email = input("Email: ").lower()
+            emails[email] = confirm_name
+        email = input("Email: ").lower()
     for email in emails:
         print(f"{emails.get(email)} ({email})")
+
 
 main()
